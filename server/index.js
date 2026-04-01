9f43cfb4623984f5055410014f38eb7c068b42bc
MatthewBeekhoo1 <matthew.beekhoo@my.uwi.edu>
2026-04-01 11:09:14 -0400

    Prepare backend for deployment

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 const taskRoutes = require("./routes/tasks");
@@ -23,6 +25,8 @@ app.get("/", (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("MongoDB connected");
-  app.listen(5000, () => console.log("Server running on port 5000"));
+  const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
 .catch(err => console.log(err));
